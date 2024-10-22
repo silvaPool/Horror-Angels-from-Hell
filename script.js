e49923944b1b036382ab8c6e40e7e9da5b1a8014
silvaPool <silva.friends22@gmail.com>
2024-10-22 10:41:34 -0300

    pequenas modificacoes no css dos cards

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const lazyElements = document.querySelectorAll('[data-background-image]');
+  const lazyElements = document.querySelectorAll('[data-background-image]');
 
-    const observer = new IntersectionObserver((entries, observer) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          const element = entry.target;
-          element.style.backgroundImage = element.getAttribute('data-background-image');
-          observer.unobserve(element); // Parar de observar depois de carregar a imagem
-        }
-      });
-    });
-
-    lazyElements.forEach(el => {
-      observer.observe(el);
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        const element = entry.target;
+        // Certifique-se de que o valor atribuído ao background-image esteja no formato correto
+        const backgroundImage = element.getAttribute('data-background-image');
+        element.style.backgroundImage = backgroundImage;
+        observer.unobserve(element); // Parar de observar depois de carregar a imagem
+      }
     });
   });
+
+  lazyElements.forEach(el => {
+    observer.observe(el);
+  });
+});
